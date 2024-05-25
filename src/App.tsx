b7c6ebaf82e44006d68df43fb3618d2b12212a48
Luisa Novaes <luisanovaes.arq@gmail.com>
2024-05-25 18:50:53 -0300

    RS-02 add: form validation and toast to sign-in page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './global.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
+import { Toaster } from 'sonner'
 
 export function App() {
   return (
     <>
     <RouterProvider router={router} />
+    <Toaster richColors/>
     </>
   )
 }
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,8 +1,43 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+
+
+import {z} from 'zod'
+
+const signInForm = z.object({   // js library to create type-safe schemas to validate data
+  email: z.string().email(),
+})
+
+type SignInForm = z.infer<typeof signInForm>  //z.infer extracts the type from the form.  SignInForm is the TS type to be used.
 
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>()
+
+  async function handleSignIn(data: SignInForm) {
+    try {
+      console.log(data)
+
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+  
+      toast.success('A link has been sent to your email',{
+        action: {
+          label: 'Resend',
+          onClick: () => handleSignIn(data)
+        },
+      })
+    } catch (error) {
+      toast.error('Invalid credentials')
+    }
+   
+  }
+
     return (
         <>
           <div className="p-8">
@@ -16,13 +51,13 @@ export function SignIn() {
                 </p>
               </div>
     
-              <form className="space-y-4">
+              <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
                 <div className="space-y-2">
                   <Label htmlFor="email">Your e-mail:</Label>
-                  <Input id="email" type="email" />
+                  <Input id="email" type="email" {...register('email')}/>
                 </div>
     
-                <Button className="w-full" type="submit">
+                <Button disabled={isSubmitting} className="w-full" type="submit">
                   Access Dashboard
                 </Button>
               </form>
