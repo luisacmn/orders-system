1e15738e864504223e986f5af119512fc48e5605
Luisa Novaes <luisanovaes.arq@gmail.com>
2024-05-25 18:23:50 -0300

    RS-02 add: create login page

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,7 +1,33 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 export function SignIn() {
     return (
         <>
-            <h1>Sign In</h1>
+          <div className="p-8">
+            <div className="flex w-[350px] flex-col justify-center gap-6">
+              <div className="flex flex-col gap-2 text-center">
+                <h1 className="text-2xl font-semibold tracking-tight">
+                  Access Dashboard
+                </h1>
+                <p className="text-sm text-muted-foreground">
+                  Manage your sales on partner dashboard
+                </p>
+              </div>
+    
+              <form className="space-y-4">
+                <div className="space-y-2">
+                  <Label htmlFor="email">Your e-mail:</Label>
+                  <Input id="email" type="email" />
+                </div>
+    
+                <Button className="w-full" type="submit">
+                  Access Dashboard
+                </Button>
+              </form>
+            </div>
+          </div>
         </>
-    )
+      )
 }
