9eea42508d7df6c46c7a9d3be3dcf004ea0f6fdd
Luisa Novaes <luisanovaes.arq@gmail.com>
2024-05-26 12:08:19 -0300

    RS-02 add: add orders page and table

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,20 +4,24 @@ import { SignIn } from "./pages/auth/sign-in";
 import { AppLayout } from "./pages/layouts/app";
 import { AuthLayout } from "./pages/layouts/auth";
 import { SignUp } from "./pages/auth/sign-up";
+import { Orders } from "./pages/app/orders/orders";
 
 export const router = createBrowserRouter([
-    { 
-        path: "/", 
-        element: <AppLayout />, 
-        children:[{ path: "/", element: <Dashboard /> }]
-    },
-    
-    { 
-        path: "/", 
-        element: <AuthLayout />, 
-        children:[
-            { path: "/sign-in", element: <SignIn /> },
-            { path: "/sign-up", element: <SignUp /> },
-        ] 
-    },
-  ]);
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      { path: "/", element: <Dashboard /> },
+      { path: "/orders", element: <Orders /> },
+    ],
+  },
+
+  {
+    path: "/",
+    element: <AuthLayout />,
+    children: [
+      { path: "/sign-in", element: <SignIn /> },
+      { path: "/sign-up", element: <SignUp /> },
+    ],
+  },
+]);
