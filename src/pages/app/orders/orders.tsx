d67b85c8eda91887b5ab1a8dd002677c43073e0a
Luisa Novaes <luisanovaes.arq@gmail.com>
2024-06-02 11:25:17 -0300

    RS-02 add: add pagination

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -7,36 +7,39 @@ import {
 } from "@/components/ui/table";
 import { OrderTableRow } from "./OrderTableRow";
 import { OrderTableFiltes } from "./OrderTableFiltes";
+import { Pagination } from "@/components/pagination";
 
 export function Orders() {
   return (
     <>
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Orders</h1>
-      </div>
-      <div className="space-y-2.5">
-        <OrderTableFiltes />
+        <div className="space-y-2.5">
+          <OrderTableFiltes />
 
-        <div className="rounded-md border">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="w-[64px]"></TableHead>
-                <TableHead className="w-[140px]">Id</TableHead>
-                <TableHead className="w-[180px]">Ordered</TableHead>
-                <TableHead className="w-[140px]">Status</TableHead>
-                <TableHead>Client</TableHead>
-                <TableHead className="w-[140px]">Total</TableHead>
-                <TableHead className="w-[164px]"></TableHead>
-                <TableHead className="w-[132px]"></TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {Array.from({ length: 10 }).map((_, i) => {
-                return <OrderTableRow key={i} />;
-              })}
-            </TableBody>
-          </Table>
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[64px]"></TableHead>
+                  <TableHead className="w-[140px]">Id</TableHead>
+                  <TableHead className="w-[180px]">Ordered</TableHead>
+                  <TableHead className="w-[140px]">Status</TableHead>
+                  <TableHead>Client</TableHead>
+                  <TableHead className="w-[140px]">Total</TableHead>
+                  <TableHead className="w-[164px]"></TableHead>
+                  <TableHead className="w-[132px]"></TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {Array.from({ length: 10 }).map((_, i) => {
+                  return <OrderTableRow key={i} />;
+                })}
+              </TableBody>
+            </Table>
+          </div>
+
+          <Pagination pageIndex={0} totalCount={105} perPage={10} />
         </div>
       </div>
     </>
